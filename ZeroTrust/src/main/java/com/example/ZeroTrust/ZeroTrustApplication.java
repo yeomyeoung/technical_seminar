24936633d982733fd22a47d21404089df0791fb2
moonstone0514 <comengiee852@naver.com>
2025-07-18 16:02:00 +0900

    maver change

--- ZeroTrust/src/main/java/com/example/ZeroTrust/ZeroTrustApplication.java
+++ ZeroTrust/src/main/java/com/example/ZeroTrust/ZeroTrustApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ZeroTrust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ZeroTrustApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ZeroTrustApplication.class, args);
 	}
 
 }
--- ZeroTrust/src/test/java/com/example/ZeroTrust/ZeroTrustApplicationTests.java
+++ ZeroTrust/src/test/java/com/example/ZeroTrust/ZeroTrustApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.ZeroTrust;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ZeroTrustApplicationTests {
 
 	@Test
 	void contextLoads() {
